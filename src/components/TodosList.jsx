4eb159f2bd4d3d8a4955185bd15e13de17937340
gdumani <gdumani@gmail.com>
2023-03-31 13:03:32 -0600

    add event handling to check todo items

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,11 +1,16 @@
-import TodoItem from '@/components/TodoItem';
-const TodosList = ({ todosProps }) => {
+import TodoItem from "@/components/TodoItem";
+const TodosList = ({ todosProps, handleChange }) => {
   return (
     <ul>
       {todosProps.map((todo) => (
-        <TodoItem key={todo.id} itemProp={todo} />
+        <TodoItem
+          key={todo.id}
+          itemProp={todo}
+          handleChange={handleChange}
+        />
       ))}
     </ul>
   );
 };
 export default TodosList;
+
